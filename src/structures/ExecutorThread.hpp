a7c769e9240c46bf19b033c69488e82ba35c1929
Disa Mhembere <dmhembe1@jhu.edu>
2017-11-30 03:22:26 +0000

    dev: stubbing classes

---
+++ src/structures/ExecutorThread.hpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2017 Neurodata (https://neurodata.io)
+ * Written by Disa Mhembere (disa@cs.jhu.edu)
+ *
+ * This file is part of Monya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef EXECUTOR_THREAD_HPP__
+#define EXECUTOR_THREAD_HPP__
+
+namespace monya { namespace structures {
+    class ExecutorThread {
+
+    };
+}} // End monya::structures
+
+#endif
