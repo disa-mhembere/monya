7c3fdd487b07e81d0ccd9c29114facc6f7a247df
Disa Mhembere <dmhembe1@jhu.edu>
2018-05-29 21:59:44 +0000

    struct: How users can as about proximity

---
+++ src/structures/SampleVector.hpp
@@ -0,0 +1,96 @@
+/*
+ * copyright 2017 neurodata (https://neurodata.io)
+ * written by disa mhembere (disa@cs.jhu.edu)
+ *
+ * this file is part of monya.
+ *
+ * licensed under the apache license, version 2.0 (the "license");
+ * you may not use this file except in compliance with the license.
+ * you may obtain a copy of the license at
+ *
+ *     http://www.apache.org/licenses/license-2.0
+ *
+ * unless required by applicable law or agreed to in writing, software
+ * distributed under the license is distributed on an "as is" basis,
+ * without warranties or conditions of any kind, either express or implied.
+ * see the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+#ifndef MONYA_SAMPLE_VECTOR_HPP__
+#define MONYA_SAMPLE_VECTOR_HPP__
+
+#include <vector>
+#include "../common/types.hpp"
+#include "../io/IO.hpp"
+
+namespace monya { namespace container {
+
+    // An abstraction over a vector OR sparse vector
+    class SampleVector {
+        public:
+            typedef std::shared_ptr<SampleVector> ptr;
+
+            SampleVector() { }
+
+            virtual data_t& operator[](size_t) = 0;
+            virtual size_t size() = 0;
+            virtual size_t nnz() = 0;
+            virtual bool empty() = 0;
+            virtual void populate(data_t*, const size_t) { }
+            virtual void print() = 0;
+    };
+
+    class DenseVector : public SampleVector {
+        private:
+            std::vector<data_t> data;
+
+        public:
+
+            static ptr create() {
+                return SampleVector::ptr(new DenseVector);
+            }
+
+            static ptr create(data_t* data, size_t nelem) {
+                return SampleVector::ptr(new DenseVector(data, nelem));
+            }
+
+            static std::shared_ptr<DenseVector> cast2(ptr o) {
+                return std::static_pointer_cast<DenseVector>(o);
+            }
+
+            DenseVector() {
+            }
+
+            DenseVector(data_t* data, size_t nelem) {
+                populate(data, nelem);
+            }
+
+            data_t& operator[](size_t idx) {
+                return data[idx];
+            }
+
+            size_t size() {
+                return data.size();
+            }
+
+            bool empty() {
+                return data.empty();
+            }
+
+            size_t nnz() {
+                return data.size();
+            }
+
+            void populate(data_t* _data, const size_t nelem) {
+                data.resize(nelem);
+                std::copy(_data, _data+nelem, data.begin());
+            }
+
+            void print() override {
+                monya::io::print_arr(&data[0], data.size());
+            }
+    };
+} }
+
+#endif
