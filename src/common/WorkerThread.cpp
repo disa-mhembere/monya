fbbd9a94b0572553f0ef85a927b008070bfe4f7d
Disa Mhembere <dmhembe1@jhu.edu>
2018-07-25 08:55:53 +0000

    thread: worker thread prelim addition

---
+++ src/common/WorkerThread.cpp
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2017 Neurodata (https://neurodata.io)
+ * Written by Disa Mhembere (disa@cs.jhu.edu)
+ *
+ * This file is part of Monya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace monya {
+
+    WorkerThread::WorkerThread(const int _node_id, const unsigned _thd_id) :
+        node_id(_node_id), thd_id(_thd_id), state(WAIT) {
+
+        pthread_mutexattr_init(&mutex_attr);
+        pthread_mutexattr_settype(&mutex_attr, PTHREAD_MUTEX_ERRORCHECK);
+        pthread_mutex_init(&mutex, &mutex_attr);
+        pthread_cond_init(&cond, NULL);
+    }
+
+    void WorkerThread::lock_sleep() {
+        int rc;
+        rc = pthread_mutex_lock(&mutex);
+        if (rc) perror("pthread_mutex_lock");
+
+        //(*parent_pending_threads)--;
+        set_thread_state(WAIT);
+
+        //if (*parent_pending_threads == 0) {
+            //rc = pthread_cond_signal(parent_cond); // Wake up parent thread
+            //if (rc) perror("pthread_cond_signal");
+        //}
+        rc = pthread_mutex_unlock(&mutex);
+        if (rc) perror("pthread_mutex_unlock");
+    }
+
+    // Assumes caller has lock already ... or else ...
+    void WorkerThread::sleep() {
+        //(*parent_pending_threads)--;
+        set_thread_state(WAIT);
+
+        //if (*parent_pending_threads == 0) {
+            //int rc = pthread_cond_signal(parent_cond); // Wake up parent thread
+            //if (rc) perror("pthread_cond_signal");
+        //}
+    }
+
+    void WorkerThread::run() {
+        switch(state) {
+            case TEST:
+                test();
+                lock_sleep();
+                break;
+            case BUILD:
+                ; // TODO
+                break;
+            case QUERY:
+                ; // TODO
+                break;
+            case WAIT:
+                ; // TODO
+                request_task();
+                break;
+            case EXIT:
+                throw thread_exception(
+                        "Thread state is EXIT but running!\n");
+            default:
+                throw thread_exception("Unknown thread state\n");
+        }
+    }
+
+    void WorkerThread::start(const ThreadState_t state) {
+        //printf("Thread %d started ...\n", thd_id);
+        this->state = state;
+        int rc = pthread_create(&hw_thd, NULL, callback, this);
+        if (rc)
+            throw thread_exception(
+                    "Thread creation (pthread_create) failed!", rc);
+    }
+
+    void WorkerThread::set_driver(void* driver) {
+        throw not_implemented_exception(__FILE__, __LINE__);
+    }
+
+    void WorkerThread::wake(const ThreadState_t state) {
+        int rc;
+        rc = pthread_mutex_lock(&mutex);
+        if (rc) perror("pthread_mutex_lock");
+        set_thread_state(state);
+
+        // TODO: Implement Logic
+
+        rc = pthread_mutex_unlock(&mutex);
+        if (rc) perror("pthread_mutex_unlock");
+
+        rc = pthread_cond_signal(&cond);
+        if (rc) perror("pthread_cond_signal");
+    }
+
+    void* callback(void* arg) {
+        WorkerThread* t = static_cast<WorkerThread*>(arg);
+#ifdef USE_NUMA
+        t->bind2node_id();
+#endif
+
+        while (true) { // So we can receive task after task
+            if (t->get_state() == WAIT)
+                t->wait();
+
+            if (t->get_state() == EXIT) {// No more work to do
+                //printf("Thread %d exiting ...\n", t->thd_id);
+                break;
+            }
+
+            //printf("Thread %d awake and doing a run()\n", t->thd_id);
+            t->run(); // else
+        }
+
+        // We've stopped running so exit
+        pthread_exit(NULL);
+#ifdef _WIN32
+        return NULL;
+#endif
+    }
+
+    bool WorkerThread::try_steal_task() {
+        return false; // TODO: Not done
+    }
+
+    const void WorkerThread::print_task_queue() const {
+        task_queue->print();
+    }
+
+    void WorkerThread::wait() {
+        int rc;
+        rc = pthread_mutex_lock(&mutex);
+        if (rc) perror("pthread_mutex_lock");
+
+        while (state == WAIT) {
+            //printf("Thread %d begin cond_wait\n", thd_id);
+            rc = pthread_cond_wait(&cond, &mutex);
+            if (rc) perror("pthread_cond_wait");
+        }
+
+        pthread_mutex_unlock(&mutex);
+    }
+
+    void WorkerThread::bind2node_id() {
+#ifdef USE_NUMA
+        struct bitmask *bmp = numa_allocate_nodemask();
+        numa_bitmask_setbit(bmp, node_id);
+        numa_bind(bmp);
+        numa_free_nodemask(bmp);
+#endif
+    }
+
+    void WorkerThread::join() {
+        void* join_status;
+        int rc = pthread_join(hw_thd, &join_status);
+        if (rc)
+            throw thread_exception("pthread_join()", rc);
+
+        thd_id = INVALID_THD_ID;
+    }
+
+    WorkerThread::~WorkerThread() {
+        pthread_cond_destroy(&cond);
+        pthread_mutex_destroy(&mutex);
+        pthread_mutexattr_destroy(&mutex_attr);
+
+        if (thd_id != INVALID_THD_ID)
+            join();
+    }
+}
---
+++ src/common/WorkerThread.hpp
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2017 Neurodata (https://neurodata.io)
+ * Written by Disa Mhembere (disa@cs.jhu.edu)
+ *
+ * This file is part of Monya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MONYA_WORKER_THREAD_HPP__
+#define MONYA_WORKER_THREAD_HPP__
+
+#include <pthread.h>
+
+#include <memory>
+#include <utility>
+#include <atomic>
+#include <string>
+#include <cassert>
+
+#include "ThreadState.hpp"
+#include "exception.hpp"
+
+#define INVALID_THD_ID -1
+
+namespace monya {
+class TaskQueue;
+
+class WorkerThread {
+protected:
+    pthread_t hw_thd;
+    unsigned node_id; // Which NUMA node are you on?
+    int thd_id;
+    TaskQueue* task_queue;
+
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    pthread_mutexattr_t mutex_attr;
+
+    pthread_cond_t* parent_cond;
+    std::atomic<unsigned>* parent_pending_threads;
+    ThreadState_t state;
+
+    friend void* callback(void* arg);
+
+    void set_thread_state(const ThreadState_t state) {
+        this->state = state;
+    }
+
+public:
+    typedef WorkerThread* raw_ptr;
+
+    WorkerThread() { }
+    WorkerThread(const int node_id, const unsigned thd_id);
+    void start(const ThreadState_t state);
+    virtual void run();
+    virtual void sleep();
+    virtual void wait();
+    virtual void join();
+
+    // Class that
+    virtual void set_driver(void* driver);
+    virtual void wake(const ThreadState_t state);
+    virtual bool try_steal_task();
+    virtual TaskQueue* get_task_queue();
+    virtual const void print_task_queue() const;
+    void bind2node_id();
+
+    void test() {
+        printf("Test method for thd: %d, NUMA node: %d\n", thd_id, node_id);
+    }
+
+    const ThreadState_t get_state() const {
+        return state;
+    }
+
+    const unsigned get_thd_id() const {
+        return thd_id;
+    }
+
+    pthread_mutex_t& get_lock() {
+        return mutex;
+    }
+
+    pthread_cond_t& get_cond() {
+        return cond;
+    }
+
+    unsigned get_node_id() {
+        return node_id;
+    }
+
+    void set_parent_cond(pthread_cond_t* cond) {
+        parent_cond = cond;
+    }
+
+    void set_parent_pending_threads(std::atomic<unsigned>* ppt) {
+        parent_pending_threads = ppt;
+    }
+
+    ~WorkerThread();
+};
+}
+#endif
